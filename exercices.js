eee977abfe1e4fbe595bbf9007df7d6f5bd8cc46
DuanrA <arnaudscrivo.dev@proton.me>
2025-02-10 09:08:52 +0100

    remove

---
+++ exercices.js
@@ -0,0 +1,7 @@
+let element = document.querySelector("#description_boutique");
+var newElement = document.createElement("p");
+var textNode = document.createTextNode("Pour cela, il vous suffit de cliquer sur une des potions de la liste ci-dessous pour l'acheter.");
+document.body.appendChild(p);
+
+element = document.querySelector("#blague_de_roger_le_sorcier");
+element.remove();
